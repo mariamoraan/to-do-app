f415278ee44edb0e940b9de67c5de678e1527e72
mia-moran <mariamoranluaces@gmail.com>
2021-10-20 14:01:17 +0200

    global styles and folder structure create

--- src/components/Hola.jsx
+++ src/components/Hola.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import '../styles/global.css';
+
 const Hola = () =>{
     return <h1>Hola mundo</h1>;
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Hola from './Hola';
+import Hola from './components/Hola';
 
 ReactDOM.render(
     <Hola />, 
